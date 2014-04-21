8a81248f3f95e66597bac6f63b23dd118831520a
Cris <cris>
2014-04-21 15:06:55 -0300

    varifica quantidade de arquivos de entrada

--- cifradorsubstituicao.c
+++ cifradorsubstituicao.c
@@ -2,10 +2,14 @@
 #include <stdlib.h>
 
 int main (int argc, char *argv[]){
+
+    if(argc<3){
+               printf("quantidades de arquivos insulficiente\n");
+               }
     
     
-    
-    
+    //no linux é outro comando
+    system("pause");
     return 0;
 
 }
