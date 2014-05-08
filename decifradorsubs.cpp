c5535dcd474182c6aedc9641b3d2e82b0253b1bf
Cris <cris>
2014-05-07 22:51:36 -0300

    melhorias quebra

--- decifradorsubs.cpp
+++ decifradorsubs.cpp
@@ -32,6 +32,7 @@ void percorrer(string cifrado){
             for(unsigned int j = 0; j < possiveisComputador.size() ;j++){
 			if(possiveisComputador[j][4]==possiveisSeguranca[i][3]
              &&possiveisComputador[j][9]==possiveisSeguranca[i][4]
+             &&possiveisComputador[j][6]==possiveisSeguranca[i][5]
              &&possiveisComputador[j][0]==possiveisSeguranca[i][7]){
                     //aqui gerar um alfabeto possivel
                     cout<<"sim"<<endl;
