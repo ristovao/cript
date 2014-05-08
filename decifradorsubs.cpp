7ba33559647f674f6542104a38df0e2987b0261c
Cris <cris>
2014-05-07 22:38:26 -0300

    melhorias

--- decifradorsubs.cpp
+++ decifradorsubs.cpp
@@ -8,21 +8,35 @@ void percorrer(string cifrado){
 	vector<string> possiveisComputador;
 	vector<string> possiveisSeguranca;
 	string substring;
-	for(unsigned int i = 0; i < cifrado.length()-10; i++){
-		substring = cifrado.substr(i, 10);
-		//checa se a substring pode ser "computador"
-		if(substring[1] == substring[8]){
-			possiveisComputador.push_back(substring);
-		}
+	if(cifrado.size()>=10){
+        for(unsigned int i = 0; i < cifrado.length()-9; i++){
+            substring = cifrado.substr(i, 10);
+            //checa se a substring pode ser "computador"
+            if(substring[1] == substring[8]){
+                possiveisComputador.push_back(substring);
+            }
+        }
 	}
-	
-	for(unsigned int i = 0; i < cifrado.length()-9; i++){
-		substring = cifrado.substr(i, 9);
-		//checa se a substring pode ser "seguranca"
-		if(substring[5] == substring[8]){
-			possiveisSeguranca.push_back(substring);
-		}
+	if(cifrado.size()>=9){
+        for(unsigned int i = 0; i < cifrado.length()-8; i++){
+            substring = cifrado.substr(i, 9);
+            //checa se a substring pode ser "seguranca"
+            if(substring[5] == substring[8]){
+                possiveisSeguranca.push_back(substring);
+            }
+        }
 	}
+
+
+	for(unsigned int i = 0; i < possiveisSeguranca.size() ;i++){
+            for(unsigned int j = 0; j < possiveisComputador.size() ;j++){
+			if(possiveisComputador[j][4]==possiveisSeguranca[i][3]){
+                    //aqui gerar um alfabeto possivel
+                    cout<<"sim"<<endl;
+                }
+            }
+	}
+
 }
 
 int main(){
